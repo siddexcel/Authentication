6d9949115426413a6a59657f544f9f73a01c284e
reactexcel <arun.etech2011@gmail.com>
2021-05-10 20:13:59 +0530

    Api integration

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
@@ -5,8 +6,17 @@ import "./LoginForm.css";
 
 const LoginForm = () => {
   const history = useHistory();
-
+  const [email, setEmail] = React.useState("");
   const handleSubmit = () => {
+    axios
+      .post("https://aqueous-thicket-08050.herokuapp.com/api/send-otp", {
+        email: email,
+      })
+      .then((value: any) => {
+        if (value && value.data && value.data.token) {
+          localStorage.setItem("token", value.data.token);
+        }
+      });
     history.push("/verify");
   };
 
@@ -15,15 +25,18 @@ const LoginForm = () => {
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail" className="m-3">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword" className="m-3">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         <div className="d-flex justify-content-center">
           <Button type="submit" className="btn-warning rounded-pill">
--- src/Components/Verification/VerificationForm.tsx
+++ src/Components/Verification/VerificationForm.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 import "./VerificationForm.css";
 
 const VerificationForm = () => {
   const [code, setCode] = React.useState("");
   const history = useHistory();
-
+  const response = React.useRef({});
+    console.log(localStorage.getItem("token"));
+    
   const handleSubmit = () => {
+    let token = localStorage.getItem("token");
+    let payload = {
+      otp: code,
+      token: token,
+    };
+    response.current = axios.post(
+      "https://aqueous-thicket-08050.herokuapp.com/api/verifyUser",
+      payload
+    );
+
     history.push("/");
   };
+
+  console.log(response.current)
 
   return (
     <div className="verification_form bg-white rounded shadow p-4">
@@ -17,7 +32,7 @@ const VerificationForm = () => {
         <Form.Group controlId="formBasicEmail" className="m-3">
           <Form.Label>Enter the 6 digit code</Form.Label>
           <Form.Control
-            type="password"
+            type="text"
             placeholder="6 Digit Code"
             required
             value={code}
