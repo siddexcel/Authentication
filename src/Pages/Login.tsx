be9e702824d792341416c753cf932906c96a8403
reactexcel <arun.etech2011@gmail.com>
2021-05-11 13:17:52 +0530

    Dashboard&logout

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import LoginForm from "../Components/LoginForm/LoginForm";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const history = useHistory();
+  const handleLoggedInUser = () => {
+    history.push("/home");
+  }
+  
+  React.useEffect(()=>{
+    if(localStorage.getItem("token")){
+      handleLoggedInUser();
+    }
+  })
+
   return (
     <div className="d-flex justify-content-center">
       <LoginForm />
