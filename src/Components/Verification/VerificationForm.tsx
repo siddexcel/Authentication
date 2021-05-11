36c36884cd7f6d004437d5338baf7f833d7f9dd7
reactexcel <arun.etech2011@gmail.com>
2021-05-11 13:00:52 +0530

    Dashboard&logout

--- src/Components/Verification/VerificationForm.tsx
+++ src/Components/Verification/VerificationForm.tsx
@@ -8,25 +8,39 @@ const VerificationForm = () => {
   const [code, setCode] = React.useState("");
   const history = useHistory();
   const response = React.useRef({});
-    console.log(localStorage.getItem("token"));
-    
+
   const handleSubmit = () => {
     let token = localStorage.getItem("token");
     let payload = {
       otp: code,
       token: token,
     };
-    response.current = axios.post(
-      "https://aqueous-thicket-08050.herokuapp.com/api/verifyUser",
-      payload
-    );
-
-    history.push("/");
+    axios
+      .post(
+        "https://aqueous-thicket-08050.herokuapp.com/api/verifyUser",
+        payload
+      )
+      .then((value) => {
+        response.current = value;
+        if (value.data.success) {
+          history.push("/home");
+        } else {
+          alert("Incorrect otp, Please try again.");
+          history.push("/");
+        }
+        console.log(value);
+      });
+    console.log(response);
   };
 
   return (
     <div className="verification_form bg-white rounded shadow p-4">
-      <Form onSubmit={handleSubmit}>
+      <Form
+        onSubmit={(e) => {
+          handleSubmit();
+          e.preventDefault();
+        }}
+      >
         <Form.Group controlId="formBasicEmail" className="m-3">
           <Form.Label>Enter the 6 digit code</Form.Label>
           <Form.Control
